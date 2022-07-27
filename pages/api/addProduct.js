fc327208e4c4547b169fb1ecc3dcac5aecac2df4
raghav <raghavjindal0212@gmail.com>
2022-07-27 19:36:27 +0530

    add product

--- pages/api/addProduct.js
+++ pages/api/addProduct.js
@@ -24,6 +24,7 @@ export default function handler(req, res) {
       console.error(error);
       res.status(500).json({ error: "error adding product" });
       return;
+      
     }
   } else {
     res.status(405).send(`Method ${req.method} not allowed`);
